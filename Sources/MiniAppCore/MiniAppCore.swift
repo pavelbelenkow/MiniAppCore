2e79db40f1a1c552f970ca3e359ae7e2f67cf8f6
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-07 13:21:55 +0300

    Implemented MiniAppProtocol and MiniAppConfiguration

--- Sources/MiniAppCore/MiniAppCore.swift
+++ Sources/MiniAppCore/MiniAppCore.swift
@@ -1,2 +1,21 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+public protocol MiniAppProtocol {
+    var title: String { get }
+    var icon: UIImage { get }
+    var view: UIView { get }
+    
+    func configure(with configuration: MiniAppConfiguration)
+}
+
+public struct MiniAppConfiguration {
+    public var backgroundColor: UIColor
+    public var textColor: UIColor
+    public var font: UIFont
+    
+    public init(backgroundColor: UIColor, textColor: UIColor, font: UIFont) {
+        self.backgroundColor = backgroundColor
+        self.textColor = textColor
+        self.font = font
+    }
+}
